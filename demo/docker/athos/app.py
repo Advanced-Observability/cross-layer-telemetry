1d22daa300c199c6865210127c78ccdfec6a55d1
Justin Iurman <justin.iurman@uliege.be>
2022-07-05 17:54:09 +0200

    update

--- demo/docker/athos/app.py
+++ demo/docker/athos/app.py
@@ -6,6 +6,7 @@ from flask import Flask, Response, request
 from http.client import HTTPConnection
 from requests.packages import urllib3
 from werkzeug.serving import WSGIRequestHandler
+from clt_genl import CrossLayerTelemetry
 
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.proto.grpc import JaegerExporter
@@ -24,9 +25,11 @@ urllib3.connectionpool.HTTPConnection = MyHTTPConnection
 urllib3.connectionpool.HTTPConnectionPool.ConnectionCls = MyHTTPConnection
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-def trace_request(web_path, http, tracer, span_name):
+def trace_request(web_path, clt, http, tracer, span_name):
     span = tracer.start_span(span_name)
-    # span.TraceId, span.SpanId
+
+    sock = MyHTTPConnection.open_socket
+    clt.enable(sock.fileno(), span.context.trace_id, span.context.span_id)
 
     data = {
         'username': request.form.get('username'), 
@@ -43,6 +46,7 @@ def trace_request(web_path, http, tracer, span_name):
     span.end()
     # END MONITORING
 
+    clt.disable(sock.fileno())
     return res
 
 
@@ -50,7 +54,7 @@ app = Flask(__name__)
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    res = trace_request('/login', http, tracer, 'login_API')
+    res = trace_request('/login', clt, http, tracer, 'login')
 
     output = "<html><head><title>CLT demo - Login API</title></head><body>"
     output += "<h3>Login API</h3>"
@@ -66,7 +70,7 @@ def login():
 if __name__ == '__main__':
     trace.set_tracer_provider(
         TracerProvider(
-            resource=Resource.create({SERVICE_NAME: "my-helloworld-service"})
+            resource=Resource.create({SERVICE_NAME: "CLT-demo"})
         )
     )
 
@@ -78,11 +82,15 @@ if __name__ == '__main__':
 
     tracer = trace.get_tracer(__name__)
 
+    time.sleep(3)
+
     sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     sock.connect(('db02::2', 80))
 
     MyHTTPConnection.open_socket = sock
     http = MyHTTPConnection("["+ str(sock.getpeername()[0]) +"]")
+
+    clt = CrossLayerTelemetry()
 
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
     app.run(host='0.0.0.0', port=15123)
